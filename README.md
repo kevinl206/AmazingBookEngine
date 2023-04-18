6b81bc8c20fa295164f9ecf923d893d7a2d7c1bb
Kevin Lang <kevinl20@yahoo.com>
2023-04-17 20:37:54 -0700

    added apollo and boost

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # AmazingBookEngine
+### Github : https://github.com/kevinl206/AmazingBookEngine.git
+### Heroku: 
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Github](#github)
+- [Email](#email)
+- [Questions](#questions)
+  
+## Description
+    This application will allows the user to search for books from the Google's book API. It will also keep a list of books to purchase.
+[Back to top](#)
+  
+## Installation
+- First thing, first. Clone the repo.
+- Then Download and install "npm install" or "npm i".
+- After the npm install, enter this command:
+"npm start
+- After running the application you will need to open the application in your browser at localhost:3000
+ 
+
+[Back to top](#)
+  
+## Screenshot of Example
+![Image]()
+
+
+[Back to top](#)
+  
+ ## Email
+      kevinl206@yahoo.com
+[Back to top](#)
+  
+## Questions
+  any additional questions, feel free to reach out to me via [email](mailto:kevinl206@yahoo.com).
--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,45 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+// import ApolloProvider & boost
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost'
+
+const httpLink = createHttpLink({
+    uri: "/graphql",
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("id_token");
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+
+
+  function App() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={SearchBooks} />
+              <Route exact path="/saved" component={SavedBooks} />
+              <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+            </Switch>
+          </>
+        </Router>
+      </ApolloProvider>
   );
 }
 
